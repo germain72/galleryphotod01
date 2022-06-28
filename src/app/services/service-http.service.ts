30ef25fa474dfa2ef7305542c914b19e8acbe040
germain72 <a-vos-clics@wanadoo.fr>
2022-06-28 11:31:48 +0200

    d01 photo gallery V1

---
+++ src/app/services/service-http.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceHttpService<T> {
+
+  public url = 'http://localhost:3000/UserPhoto';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<T[]> {
+    return this.http.get<T[]>(this.url);
+  }
+
+  getOne(key: any): Observable<T> {
+    return this.http.get<T>(`${this.url}\${key}`);
+  }
+
+  post(data: T): Observable<T> {
+    return this.http.post<T>(this.url,data);
+  }
+
+}
